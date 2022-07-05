1d52c4d7b1ab87bbf9683d48744276661a91432f
Tole1010 <antoniokontakti@gmail.com>
2022-07-05 11:38:05 +0200

    added services route

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent implements OnInit, OnDestroy {
+  panels!: NodeListOf<Element>;
+  images!: NodeListOf<Element>;
+  imgs!: HTMLElement | null;
 
-  constructor() { }
+  interval = setInterval(() => {
+    return this.run();
+  }, 2000);
+
+  idx = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.panels = document.querySelectorAll('.panel');
+    this.images = document.querySelectorAll('#imgs img');
+    this.imgs = document.querySelector('#imgs');
   }
 
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  onPanelClick(event: any) {
+    this.panels.forEach((panel) => {
+      panel.classList.remove('active');
+    });
+    event.target.classList.add('active');
+  }
+
+  run() {
+    this.idx++;
+    this.changeImage();
+  }
+
+  changeImage() {
+    if (this.idx > this.images.length - 1) {
+      this.idx = 0;
+    } else if (this.idx < 0) {
+      this.idx = this.images.length - 1;
+    }
+  }
+
+  resetInterval() {
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      return this.run();
+    }, 2000);
+  }
+
+  nextImage() {
+    this.idx++;
+
+    this.changeImage();
+    this.resetInterval();
+  }
+
+  previousImage() {
+    this.idx--;
+
+    this.changeImage();
+    this.resetInterval();
+  }
 }
