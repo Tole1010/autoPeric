480e173d9b65bed940ec9fa6ac41698fbcd6dbbe
Tole1010 <antoniokontakti@gmail.com>
2022-07-13 14:18:09 +0200

    changes to all pages

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css'],
 })
-export class ServicesComponent implements OnInit, OnDestroy {
+export class ServicesComponent implements OnInit, OnDestroy, AfterViewInit {
   panels!: NodeListOf<Element>;
   images!: NodeListOf<Element>;
   imgs!: HTMLElement | null;
 
-  interval = setInterval(() => {
-    return this.run();
-  }, 2000);
+  interval: any;
 
   idx = 0;
 
+  imgsArray: { src: string; naziv: string }[] = [
+    {
+      src: 'https://images.unsplash.com/photo-1517524206127-48bbd363f3d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+      naziv: 'Redovan servis',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1619642751034-765dfdf7c58e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+      naziv: 'Popravak',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1599474151975-1f978922fa02?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      naziv: 'Termolakiranje',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1504222490345-c075b6008014?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      naziv: 'Održavanje',
+    },
+  ];
+
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
     this.panels = document.querySelectorAll('.panel');
     this.images = document.querySelectorAll('#imgs img');
     this.imgs = document.querySelector('#imgs');
+    this.interval = setInterval(() => {
+      return this.run();
+    }, 2000);
   }
 
   ngOnDestroy(): void {
